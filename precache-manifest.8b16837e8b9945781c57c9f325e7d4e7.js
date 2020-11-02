1cea9369589baca7daa6abeb4cb760b547937320
Nikitapopov <nikitos.popov@inbox.ru>
2020-11-02 21:42:59 +0300

    Updates

---
+++ precache-manifest.8b16837e8b9945781c57c9f325e7d4e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d932982b668aa12049808c14a575f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "995831b1ae3bdae54ca1",
+    "url": "/static/css/main.a0387641.chunk.css"
+  },
+  {
+    "revision": "d932b4112c29d8abbb47",
+    "url": "/static/js/2.0d50837a.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.0d50837a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995831b1ae3bdae54ca1",
+    "url": "/static/js/main.3cc99fa0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
